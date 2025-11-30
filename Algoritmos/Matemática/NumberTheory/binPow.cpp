3bb75f1c819d339898def72109a21b3d35a45360
Kaeljane <kayyt0102@gmail.com>
2025-11-30 18:17:51 -0300

    Att binPow

--- Algoritmos/Matemática/NumberTheory/binPow.cpp
+++ Algoritmos/Matemática/NumberTheory/binPow.cpp
@@ -18,6 +18,19 @@ long long binPow(long long a, long long b, long long m) {
     return res;
 }
 
+ll binPow(ll a, ll b, ll m) {
+    a %= m;
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1) {
+            res = (res * a) % m;
+        }
+        a = (a*a) % m;
+        b >>= 1;
+    }
+    return res;
+}
+
 /*
 Detalhes Técnicos Importantes:
 
