a89153afb716cfce45e64326dc65b40f80b8972e
Kaeljane <kayyt0102@gmail.com>
2026-01-03 15:00:03 -0300

    Add codigo fatorial

--- Algoritmos/Recursion/factorial.cpp
+++ Algoritmos/Recursion/factorial.cpp
@@ -1,11 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int factorial(int n) {
+int fact(int n) {
     // Caso base: o fatorial de 0 ou 1 é 1
     if (n == 0 || n == 1) {
         return 1;
     }
     // Chamada recursiva: n! = n * (n-1)!
-    return n * factorial(n - 1);
+    return n * fact(n - 1);
 }
