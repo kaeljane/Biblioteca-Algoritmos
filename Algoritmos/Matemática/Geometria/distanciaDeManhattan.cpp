a10450ff689f95cc598a2d876554f29a12d073f5
Kaeljane <kayyt0102@gmail.com>
2026-02-08 14:19:57 -0300

    Add Fórmula da Distância de Manhattan | BIBLIOTECA

---
+++ Algoritmos/Matemática/Geometria/distanciaDeManhattan.cpp
@@ -0,0 +1,12 @@
+/*
+Manhattan Distance (TEORIA)
+
+Num grid infinito onde você só pode andar na vertical e 
+horizontal, a distância mínima entre dois pontos A(x1, y1)
+e B(x2, y2) sem obstaculos é sempre a Distância de 
+Manhattan Dis = |x1 - x2| + |y1 - y2|
+
+*/
+
+ll dist = abs(x1 - x2) + abs(y1 - y2);
+
