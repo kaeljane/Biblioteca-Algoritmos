6eca769af073bf531268df712f1d34253df71374
Kaeljane <kayyt0102@gmail.com>
2026-05-19 00:08:32 -0300

    Add Template Basico | BIBLIOTECA

---
+++ template.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define ii pair<ll, int>
+#define vl vector<long long>
+#define vll vector<vector<long long>>
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define f(i,b,e) for (ll i = (b); i < (e); i++)
+#define rf(i,b,e) for (ll i = (b); i >= (e); i--)
+#define pb push_back
+#define el '\n'
+#define uset unordered_set
+#define ___ ios::sync_with_stdio(false); cin.tie(nullptr);
+using namespace std;
+
+ll MOD = 1e9 + 7; // 998244353;
+ll INF = 1e18 + 7;
+const int MAXN = 1e5 + 5;
+// matriz vll mat(n, vl(m, 0))
+
+ll n, t, k, x, y, z;
+string s, a, b, c;
+
+/* (mind) 
+    
+    
+*/
+void solve() {
+    cin>>n;
+    vl v(n); for (ll i = 0; i < n; i++) cin>>v[i];
+    
+    
+}
+signed main() {
+    ___
+    // cin>>t;
+    // while(t--)
+    solve();
+    
+    return 0;
+}
